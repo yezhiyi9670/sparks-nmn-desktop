16610e774c600f56229b957cc8e257f7c40c67a1
yezhiyi9670 <yezhiyi@toxic2018.ml>
2023-02-19 09:08:17 +0800

    Added hotfix from core

--- src/preload/api/Versions.ts
+++ src/preload/api/Versions.ts
@@ -1,3 +1,3 @@
 export default {
-	app: '0.3.1'
+	app: '0.3.2'
 }
--- src/renderer/nmn/renderer/article/line/PositionDispatcher.ts
+++ src/renderer/nmn/renderer/article/line/PositionDispatcher.ts
@@ -278,6 +278,7 @@ export class PositionDispatcher {
 			}
 			const noteCharMetric = getLineFont(isSmall ? 'noteSmall' : 'note', this.context)
 			const accidentalCharMetric = getLineFont(isSmall ? 'noteSmall' : 'note', this.context)
+			accidentalCharMetric.fontFamily = 'SparksNMN-Bravura'
 			const noteCharMeasure = this.root.measureTextFast('0', noteCharMetric, this.scale)
 			const accidentalMeasure = this.root.measureTextFast("\uE10E", accidentalCharMetric, this.scale)
 			const addNoteCharMeasure = [noteCharMeasure[0] * addNotesScale, 0]
