f846f8c7e35e41c904f4c1c05f815025cff582b1
yezhiyi9670 <yezhiyi@toxic2018.ml>
2023-05-12 12:33:38 +0800

    1

--- local/copy-changes.js
+++ local/copy-changes.js
@@ -8,6 +8,6 @@ if(!fs.existsSync("package.json")) {
 
 const installDir = 'D:/Program Files (portable)/sparks-nmn-desktop/'
 if(fs.existsSync(installDir)) {
-	fs.copyFileSync('out/sparks-nmn-desktop-win32-ia32/sparks-nmn-desktop.exe', installDir + 'sparks-nmn-desktop.exe')
-	fs.copyFileSync('out/sparks-nmn-desktop-win32-ia32/resources/app.asar', installDir + 'resources/app.asar')
+	fs.copyFileSync('out/sparks-nmn-desktop-win32-x64/sparks-nmn-desktop.exe', installDir + 'sparks-nmn-desktop.exe')
+	fs.copyFileSync('out/sparks-nmn-desktop-win32-x64/resources/app.asar', installDir + 'resources/app.asar')
 }
